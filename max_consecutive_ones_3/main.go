456afbed6d856257f842ff2b3ba1ce5ed360a20a
Maxim Filatov <mczim@yandex-team.ru>
2023-10-13 21:41:22 +0200

    max_consecutive_ones_3

---
+++ max_consecutive_ones_3/main.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(longestOnes([]int{1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0}, 2)) //6
+	fmt.Println(longestOnes([]int{0, 0, 1, 1, 1, 0, 0}, 0))             //3
+	fmt.Println(longestOnes([]int{0, 0, 0, 0}, 0))                      //0
+	fmt.Println(longestOnes([]int{0}, 1))                               //1
+	fmt.Println(longestOnes([]int{1, 0, 1, 0}, 0))                      //1
+}
+
+func longestOnes(nums []int, k int) int {
+	zeroCount, longestWindow, start := 0, 0, 0
+
+	//if k == 0 && zeroCount == len(nums)-1 {
+	//	return 0
+	//}
+	//if k == 0 {
+	//	for i := 0; i < len(nums); i++ {
+	//		if nums[i] == 1 {
+	//			longestWindow = max(longestWindow, nums[i])
+	//		}
+	//	}
+	//	return longestWindow
+	//}
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == 0 {
+			zeroCount++
+		}
+		for zeroCount > k {
+			if nums[start] == 0 {
+				zeroCount--
+			}
+			start++
+		}
+		longestWindow = max(longestWindow, i-start+1)
+	}
+	return longestWindow
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
