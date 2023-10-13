2c8d4cad1afbcbb0d29e96b62c7ebf7d82ffea94
Maxim Filatov <mczim@yandex-team.ru>
2023-10-13 16:31:27 +0200

    max_consecutive_ones

---
+++ max_consecutive_ones/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(findMaxConsecutiveOnes([]int{1, 1, 0, 1, 1, 1})) //3
+}
+
+func findMaxConsecutiveOnes(nums []int) int {
+	curr, res := 0, 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == 1 {
+			curr++
+		} else {
+			if curr > res {
+				res = curr
+			}
+			curr = nums[i]
+		}
+	}
+	if curr > res {
+		res = curr
+	}
+	return res
+}
