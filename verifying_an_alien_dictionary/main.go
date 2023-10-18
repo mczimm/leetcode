270f023a2c04c56edf08778211bd43fe469737e6
Maxim Filatov <mczim@yandex-team.ru>
2023-10-18 22:07:10 +0200

    verifying_an_alien_dictionary

--- verifying_an_alien_dictionary/main.go
+++ verifying_an_alien_dictionary/main.go
@@ -13,12 +13,12 @@ func isAlienSorted(words []string, order string) bool {
 		orderMap[order[i]] = i
 	}
 
-	for i := 0; i < len(words)-1; i++ {
-		for j := 0; j < len(words[i]); j++ {
+	for i := 0; i < len(words)-1; i++ { //loop over first word
+		for j := 0; j < len(words[i]); j++ { // loop over character of first word
 			if j >= len(words[i+1]) {
 				return false
-			}
-			if words[i][j] != words[i+1][j] {
+			} // if count j bigger than len of second word
+			if words[i][j] != words[i+1][j] { // if character in first word not equal character in second word
 				if orderMap[words[i][j]] > orderMap[words[i+1][j]] {
 					return false
 				} else {
