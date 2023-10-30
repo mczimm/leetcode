3a671aead6e3eef6dbe6ed50ba4dba84276947ad
Maxim Filatov <mczim@yandex-team.ru>
2023-10-30 20:04:23 +0100

    remove_element

---
+++ remove_element/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(removeElement([]int{3, 2, 2, 3}, 3))             //2
+	fmt.Println(removeElement([]int{0, 1, 2, 2, 3, 0, 4, 2}, 2)) //5
+}
+
+//My first attempt
+func removeElement(nums []int, val int) int {
+	i := 0
+	for i < len(nums) {
+		if nums[i] == val {
+			nums = append(nums[:i], nums[i+1:]...)
+		} else {
+			i++
+		}
+	}
+	return len(nums)
+}
