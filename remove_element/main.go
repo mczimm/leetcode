58a25e8d2caa8e59bcc770ab0d0fff07e76558a0
Maxim Filatov <mczim@yandex-team.ru>
2023-10-31 14:15:24 +0100

    remove_element

--- remove_element/main.go
+++ remove_element/main.go
@@ -7,7 +7,7 @@ func main() {
 	fmt.Println(removeElement([]int{0, 1, 2, 2, 3, 0, 4, 2}, 2)) //5
 }
 
-//My first attempt
+// My first attempt
 func removeElement(nums []int, val int) int {
 	i := 0
 	for i < len(nums) {
