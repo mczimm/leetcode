167d3a24ae0aaee25147b2a35e479df5b4d6f9f9
Maxim Filatov <mczim@yandex-team.ru>
2023-11-02 17:12:05 +0100

    excel_sheet_column_number

---
+++ excel_sheet_column_number/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(titleToNumber("A")) //1
+}
+
+// From the solutions
+func titleToNumber(columnTitle string) int {
+	sum := 0
+	for _, s := range columnTitle {
+		sum *= 26
+		sum += int(s) - 64
+	}
+	return sum
+}
