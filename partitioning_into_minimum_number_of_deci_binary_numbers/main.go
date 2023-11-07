70177e57baac12d2e70ee594e38c9e54d7091599
Maxim Filatov <mczim@yandex-team.ru>
2023-11-07 09:21:03 +0100

    partitioning_into_minimum_number_of_deci_binary_numbers

---
+++ partitioning_into_minimum_number_of_deci_binary_numbers/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(minPartitions("32"))    //3
+	fmt.Println(minPartitions("82734")) //8
+}
+
+// From the solutions: https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/solutions/3441947/golang/
+func minPartitions(n string) int {
+	var maxDigit uint8
+
+	for i := 0; i < len(n); i++ {
+		currDigit := n[i] - '0'
+		if currDigit > maxDigit {
+			maxDigit = currDigit
+		}
+	}
+	return int(maxDigit)
+}
