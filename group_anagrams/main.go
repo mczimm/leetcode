3e7cf539da4c5b9de9b3c86e584fdc554bcea646
Maxim Filatov <mczim@yandex-team.ru>
2023-11-27 21:13:02 +0100

    group_anagrams

---
+++ group_anagrams/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	//[["bat"],["nat","tan"],["ate","eat","tea"]]
+	fmt.Println(groupAnagrams([]string{"eat", "tea", "tan", "ate", "nat", "bat"}))
+	//[["max"],["buy"],["doc"],["may"],["ill"],["duh"],["tin"],["bar"],["pew"],["cab"]]
+	fmt.Println(groupAnagrams([]string{"cab", "tin", "pew", "duh", "may", "ill", "buy", "bar", "max", "doc"}))
+	fmt.Println(groupAnagrams([]string{"duh", "ill"})) //same sum
+}
+
+// My first attempt
+func groupAnagrams(strs []string) [][]string {
+	tp := make(map[[26]int][]string)
+	var res [][]string
+
+	for i := 0; i < len(strs); i++ {
+		var ts [26]int
+		// I've given it from solution
+		for j := 0; j < len(strs[i]); j++ {
+			ts[strs[i][j]-'a']++
+		}
+		// End
+		tp[ts] = append(tp[ts], strs[i])
+	}
+	for _, v := range tp {
+		res = append(res, v)
+	}
+	return res
+}
