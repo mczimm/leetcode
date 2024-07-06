cfcf64569acdc8fbc9c056fda5a2baea3cbfe952
Max Filatov <mczimm@gmail.com>
2024-07-06 11:20:23 +0200

    merge_two_sorted_lists

--- merge_two_sorted_lists/main.go
+++ merge_two_sorted_lists/main.go
@@ -8,19 +8,16 @@ type ListNode struct {
 }
 
 func main() {
-	five1 := &ListNode{Val: 5}
-	four1 := &ListNode{Val: 4, Next: five1}
-	three1 := &ListNode{Val: 3, Next: four1}
-	two1 := &ListNode{Val: 2, Next: three1}
+	three1 := &ListNode{Val: 4}
+	two1 := &ListNode{Val: 3, Next: three1}
 	head1 := &ListNode{Val: 1, Next: two1}
 
-	five2 := &ListNode{Val: 5}
-	four2 := &ListNode{Val: 4, Next: five2}
-	three2 := &ListNode{Val: 3, Next: four2}
+	three2 := &ListNode{Val: 4}
 	two2 := &ListNode{Val: 2, Next: three2}
 	head2 := &ListNode{Val: 1, Next: two2}
 
 	res := mergeTwoLists(head1, head2)
+
 	fmt.Println(res)
 }
 
@@ -40,6 +37,8 @@ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 		merged = merged.Next
 	}
 
+	//the list1 and list2 two can be different length
+	//that's why we need to check it one more time separately
 	for list1 != nil {
 		merged.Next = &ListNode{Val: list1.Val}
 		list1 = list1.Next
