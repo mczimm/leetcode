4da91c96b94195a182f14a9151d4e847b57bafd1
Max Filatov <mczimm@gmail.com>
2024-07-24 13:46:32 +0500

    meeting_rooms

--- meeting_rooms/main.go
+++ meeting_rooms/main.go
@@ -5,18 +5,18 @@ determine if a person could aatend all meetings.
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	fmt.Println(canAttendMeetings([][]int{{0, 30}, {5, 10}, {15, 20}})) //false
 	fmt.Println(canAttendMeetings([][]int{{7, 10}, {2, 4}}))            //true
+	fmt.Println(canAttendMeetings([][]int{{13, 15}, {1, 13}}))          //true
 }
 
-/*
-THIS PROBLEM UNDER PREMIUM ACCOUNT SO I CAN'T CHECK IT ON LEETCODE
-*/
-
-func canAttendMeetings(n [][]int) bool {
+func canAttendMeetings2(n [][]int) bool {
 	nSorted := bubbleSort(n)
 	for i := 0; i < len(nSorted)-1; i++ {
 		if nSorted[i][1] < nSorted[i+1][1] {
@@ -40,4 +40,22 @@ func bubbleSort(n [][]int) [][]int {
 		}
 	}
 	return n
+}
+
+// 24.07.2024
+func canAttendMeetings(intervals [][]int) bool {
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+	fmt.Println(intervals)
+
+	for i := 0; i < len(intervals)-1; i++ {
+		if intervals[i][1] <= intervals[i+1][0] {
+			continue
+		} else {
+			return false
+		}
+	}
+
+	return true
 }
