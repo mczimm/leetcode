a935a790586abe4c18871a8c11a80e21bbe5b70e
Maxim Filatov <mczim@nebius.com>
2024-11-13 18:28:22 +0100

    best_time_to_buy_and_sell_stock

--- best_time_to_buy_and_sell_stock/main.go
+++ best_time_to_buy_and_sell_stock/main.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(maxProfit([]int{7, 1, 5, 3, 6, 4}))    //5
-	fmt.Println(maxProfit([]int{7, 6, 4, 3, 1}))       //0
-	fmt.Println(maxProfit([]int{1, 2}))                //1
-	fmt.Println(maxProfit([]int{2, 4, 1}))             //2
-	fmt.Println(maxProfit([]int{2, 1, 2, 0, 1}))       //1
-	fmt.Println(maxProfit([]int{2, 1, 2, 1, 0, 1, 2})) //2
-	fmt.Println(maxProfit([]int{3, 2, 6, 5, 0, 3}))    //4
+	fmt.Println(maxProfit2([]int{7, 1, 5, 3, 6, 4}))    //5
+	fmt.Println(maxProfit2([]int{7, 6, 4, 3, 1}))       //0
+	fmt.Println(maxProfit2([]int{1, 2}))                //1
+	fmt.Println(maxProfit2([]int{2, 4, 1}))             //2
+	fmt.Println(maxProfit2([]int{2, 1, 2, 0, 1}))       //1
+	fmt.Println(maxProfit2([]int{2, 1, 2, 1, 0, 1, 2})) //2
+	fmt.Println(maxProfit2([]int{3, 2, 6, 5, 0, 3}))    //4
 }
 
 func maxProfit(prices []int) int {
@@ -35,4 +35,20 @@ func maxProfit(prices []int) int {
 	}
 
 	return res
+}
+
+// 13.11.24
+func maxProfit2(prices []int) int {
+	var maxProfit int
+	minPrice := int(^uint(0) >> 1) //9223372036854775807
+
+	for i := 0; i < len(prices); i++ {
+		if prices[i] < minPrice {
+			minPrice = prices[i]
+		}
+		if prices[i]-minPrice > maxProfit {
+			maxProfit = prices[i] - minPrice
+		}
+	}
+	return maxProfit
 }
