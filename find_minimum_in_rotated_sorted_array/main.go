680d3beca4eb4e27b6a144e68d68490e3c81c380
Maxim Filatov <mczim@nebius.com>
2025-01-06 18:20:50 +0100

    find_minimum_in_rotated_sorted_array

---
+++ find_minimum_in_rotated_sorted_array/main.go
@@ -0,0 +1,37 @@
+package main
+
+func main() {
+	println(findMin([]int{3, 4, 5, 1, 2})) // 1
+	println(findMin([]int{3, 4, 5, 1, 2})) // 1
+}
+
+func findMin(nums []int) int {
+	left := 0
+	right := len(nums) - 1
+
+	if len(nums) == 1 {
+		return nums[0]
+	}
+
+	if nums[right] > nums[0] {
+		return nums[0]
+	}
+
+	for right >= left {
+		mid := left + (right-left)/2
+
+		if nums[mid] > nums[mid+1] {
+			return nums[mid+1]
+		}
+		if nums[mid] < nums[mid-1] {
+			return nums[mid]
+		}
+
+		if nums[mid] > nums[0] {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return -1
+}
