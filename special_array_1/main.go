5c959e1e0fe9968f1d032b07a27d2139e6351974
Maxim Filatov <mczim@nebius.com>
2025-02-01 14:13:10 +0100

    max_consecutive_ones

--- special_array_1/main.go
+++ special_array_1/main.go
@@ -1,1 +1,22 @@
-package special_array_1
+package main
+
+func main() {
+	println(isArraySpecial([]int{2, 1, 4})) //true
+	println(isArraySpecial([]int{2, 10}))   //false
+}
+
+func isArraySpecial(nums []int) bool {
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i]%2 == 0 {
+			if nums[i+1]%2 == 0 {
+				return false
+			}
+		}
+		if nums[i]%2 != 0 {
+			if nums[i+1]%2 != 0 {
+				return false
+			}
+		}
+	}
+	return true
+}
